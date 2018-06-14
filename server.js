254ba8d6cd243c5507729331e48c73759d1ee56e
Ray <Ray.lin@gigabyte.com>
2018-06-14 17:10:31 +0800

    ray
    小天使

--- server.js
+++ server.js
@@ -45,6 +45,46 @@ var userId = {};
 
 //建立一個監聽是否有使用者連進來的function
 var userData = [];  //暫存目前正連線中使用者的name/id對應表
+var angelData = {
+  "小天使": [
+    "嗨!有人呼叫我嗎? 你可以下達指令 ex:'小天使 抽卡'or'小天使 講笑話'",
+    "嗨!有人想我嗎?可是我不想你歐!!",
+    "其實我跟惡魔是好朋友!",
+    "Hi!你好!!"
+  ],
+  "小天使 抽卡": [
+    "整天只會想抽卡，你還會幹嘛?",
+    "霹靂卡霹靂拉拉....卡片不見了",
+    "看你可憐送你一張圖啦!<br><img src='http://lorempixel.com/400/200/cats' class='rounded' width='250'>",
+    "看你可憐送你一張圖啦!<br><img src='http://lorempixel.com/400/200/cats' class='rounded' width='250'>",
+    "看你可憐送你一張圖啦!<br><img src='http://lorempixel.com/400/200/cats' class='rounded' width='250'>",
+    "看你可憐送你一張圖啦!<br><img src='http://lorempixel.com/400/200/cats' class='rounded' width='250'>",
+    "看你可憐送你一張圖啦!<br><img src='http://lorempixel.com/400/200/cats' class='rounded' width='250'>"
+  ],
+  "小天使 講笑話": [
+    '不要啦!我講的笑話不好笑',
+    '好吧!你堅持的話!<br>有一天，小美走在路上，遇到一隻烏龜，小美很驚慌地說:"你是要劫財還是劫色阿!"，結果你知道烏龜說什麼嗎? "傑泥傑泥"'
+  ],
+  "小天使 天氣": [
+    '你當我氣象局?',
+    '天氣的基本釋義:在較短時間內特定地區的大氣狀況、氣象情況<br>詳細釋義:古人指輕清之氣。 《逸周書·時訓》：“小雪之日，虹藏不見。又五日，天氣上騰，地氣下降。” 泛指空氣。 太平天囯 洪仁玕 《自傳》：“鼻之呼吸，刻不能不與天氣相通。” 天命，氣數。 唐 許敬宗 《尉遲恭碑》：“ 劉武周 不稽天氣，寔暗人謀。” 氣候。 三國 魏 曹丕 《燕歌行》：“秋風蕭瑟天氣涼，草木搖落露為霜。” 宋 張先 《八寶裝》詞：“正不寒不暖，和風細雨，困人天氣。” 清 程趾祥 《此中人語·河中井》：“時天氣炎熱，游泳於河，竟失足墮下。” 丁玲 《阿毛姑娘》：“不怕天氣已很冷，沿路上還是有不少燒香的客。” 時候。指某一時刻。 《水滸傳》第八回：“兩個公人帶了 林沖 出店，卻是五更天氣。”《兒女英雄傳》第五回：“莫如趁天氣還早，躲了他。” 魏巍 《在風雪里》三：“吃過飯，天氣已經不早了。” 指一段時間。 明 馮夢龍 《掛枝兒·醉歸》：“俏冤家夜深歸，喫得爛醉……枉了奴對孤燈守了三更多天氣。” 康濯 《徐水平原的白天黑夜》：“他并不是黨員，快到六十歲，結婚才不過十年天氣。”'
+  ]
+};
+
+
+
+var angelDo = function (data) {
+  var mes = "";
+  var len = angelData[data].length;
+  var id = Math.floor((Math.random() * len));
+  if(len != 0){
+    mes = angelData[data][id];
+  }else{
+    
+  }
+  
+  return mes;
+}
 
 app.io.sockets.on('connection', function (socket) {
   // console.log('有使用者連進來');
@@ -84,6 +124,16 @@ app.io.sockets.on('connection', function (socket) {
     socket.emit('updateMe', data, userName);
     socket.broadcast.emit('updateUserList', userData);
     socket.emit('updateUserList', userData);
+
+    for (var key in angelData) {
+      if (key == data) {
+        socket.broadcast.emit('angel', angelDo(key));
+        socket.emit('angel', angelDo(key));
+
+        break;
+      }
+    }
+
   });
 
   socket.on('disconnect', function () {
