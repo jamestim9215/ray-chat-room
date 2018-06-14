0647d039f392c6b9af694233f5fba0162bd28616
Ray <Ray.lin@gigabyte.com>
2018-06-14 10:12:55 +0800

    ray
    修復動畫

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,11 +5,25 @@ $(window).load(function () {
     });
 
     $(".menuBtn").on('click', function () {
-        $(".chat-box").css({ "z-index": "0", "transform": "rotateY(-180deg)" });
-        $(".message").css({ "z-index": "2", "transform": "rotateY(0deg)" });
+        $(".chat-box").css({ "transform": "translate(50vw) scale(0.95)" });
+        $(".message").css({ "transform": "translate(-50vw) scale(0.95)" });
+        setTimeout(() => {
+            $(".chat-box").css({ "z-index": "0" });
+            setTimeout(() => {
+                $(".chat-box").css({ "transform": "translate(0vw) scale(0.9)" });
+                $(".message").css({ "transform": "translate(0vw) scale(1)" });
+            }, 400);
+        }, 100);
     });
     $(".closeBtn").on('click', function () {
-        $(".chat-box").css({ "z-index": "2", "transform": "rotateY(0deg)" });
-        $(".message").css({ "z-index": "0", "transform": "rotateY(180deg)" });
+        $(".chat-box").css({ "transform": "translate(50vw) scale(0.95)" });
+        $(".message").css({ "transform": "translate(-50vw) scale(0.95)" });
+        setTimeout(() => {
+            $(".chat-box").css({ "z-index": "2" });
+            setTimeout(() => {
+                $(".chat-box").css({ "transform": "translate(0vw) scale(1)" });
+                $(".message").css({ "transform": "translate(0vw) scale(0.9)" });
+            }, 100);
+        }, 400);
     });
 });
